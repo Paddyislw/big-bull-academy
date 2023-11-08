ad6596c25bfebb1fd1847c7d493ad56e46946980
Paddy <87908260+Paddyislw@users.noreply.github.com>
2023-11-08 18:43:05 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '243745895368796');
+            fbq('init', '1055280225656037');
             fbq('track', 'PageView');
             `,
           }}
